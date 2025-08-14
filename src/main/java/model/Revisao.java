89f26a986f96642938558e7b78507458c2d25977
Maykon Carvalho <carvalhomaykon566@gmail.com>
2025-08-14 10:01:57 -0300

    Creation of entities

---
+++ src/main/java/model/Revisao.java
@@ -0,0 +1,78 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "resivoes")
+public class Revisao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "veiculos_id")
+    private Long veiculoId;
+
+    @Column(name = "status_revisao")
+    private String statusRevisao;
+
+    @Column(name = "data_prevista_revisao")
+    private LocalDate dataPrevistaRevisao;
+
+    @Column(name = "km_fazer_revisao")
+    private Double kmFazerRevisao;
+
+    public Revisao() {}
+
+    public Revisao(Long id, Long veiculoId, String statusRevisao, LocalDate dataPrevistaRevisao, Double kmFazerRevisao) {
+        this.id = id;
+        this.veiculoId = veiculoId;
+        this.statusRevisao = statusRevisao;
+        this.dataPrevistaRevisao = dataPrevistaRevisao;
+        this.kmFazerRevisao = kmFazerRevisao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getVeiculoId() {
+        return veiculoId;
+    }
+
+    public void setVeiculoId(Long veiculoId) {
+        this.veiculoId = veiculoId;
+    }
+
+    public String getStatusRevisao() {
+        return statusRevisao;
+    }
+
+    public void setStatusRevisao(String statusRevisao) {
+        this.statusRevisao = statusRevisao;
+    }
+
+    public LocalDate getDataPrevistaRevisao() {
+        return dataPrevistaRevisao;
+    }
+
+    public void setDataPrevistaRevisao(LocalDate dataPrevistaRevisao) {
+        this.dataPrevistaRevisao = dataPrevistaRevisao;
+    }
+
+    public Double getKmFazerRevisao() {
+        return kmFazerRevisao;
+    }
+
+    public void setKmFazerRevisao(Double kmFazerRevisao) {
+        this.kmFazerRevisao = kmFazerRevisao;
+    }
+}
---
+++ src/main/java/model/Veiculo.java
@@ -0,0 +1,120 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "veiculos")
+public class Veiculo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "users_id")
+    private Long userId;
+
+    private String tipo;
+    private String modelo;
+    private Double quilometragem;
+
+    @Column(name = "quilometragem_diaria")
+    private Double quilometragemDiaria;
+
+    @Column(name = "dias_uso_semana")
+    private Integer diasUsoSemana;
+
+    @Column(name = "ultima_revisao")
+    private LocalDate ultimaRevisao;
+
+    @Column(name = "ano_fabricacao")
+    private Integer anoFabricacao;
+
+    public Veiculo() {}
+
+    public Veiculo(Long id, Long userId, String tipo, String modelo, Double quilometragem, Double quilometragemDiaria, Integer diasUsoSemana, LocalDate ultimaRevisao, Integer anoFabricacao) {
+        this.id = id;
+        this.userId = userId;
+        this.tipo = tipo;
+        this.modelo = modelo;
+        this.quilometragem = quilometragem;
+        this.quilometragemDiaria = quilometragemDiaria;
+        this.diasUsoSemana = diasUsoSemana;
+        this.ultimaRevisao = ultimaRevisao;
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Double getQuilometragem() {
+        return quilometragem;
+    }
+
+    public void setQuilometragem(Double quilometragem) {
+        this.quilometragem = quilometragem;
+    }
+
+    public Double getQuilometragemDiaria() {
+        return quilometragemDiaria;
+    }
+
+    public void setQuilometragemDiaria(Double quilometragemDiaria) {
+        this.quilometragemDiaria = quilometragemDiaria;
+    }
+
+    public Integer getDiasUsoSemana() {
+        return diasUsoSemana;
+    }
+
+    public void setDiasUsoSemana(Integer diasUsoSemana) {
+        this.diasUsoSemana = diasUsoSemana;
+    }
+
+    public LocalDate getUltimaRevisao() {
+        return ultimaRevisao;
+    }
+
+    public void setUltimaRevisao(LocalDate ultimaRevisao) {
+        this.ultimaRevisao = ultimaRevisao;
+    }
+
+    public Integer getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(Integer anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+}
