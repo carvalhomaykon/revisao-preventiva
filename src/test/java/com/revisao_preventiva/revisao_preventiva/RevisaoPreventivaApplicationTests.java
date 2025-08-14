80adb3c597128a0874afaae830cd875db53cfbe3
Maykon Carvalho <carvalhomaykon566@gmail.com>
2025-08-14 09:35:18 -0300

    first commit

---
+++ src/test/java/com/revisao_preventiva/revisao_preventiva/RevisaoPreventivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revisao_preventiva.revisao_preventiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisaoPreventivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
